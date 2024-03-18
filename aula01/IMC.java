485fae2f73a218e3d57f8ab5e8a36d36d57c48e7
Ryan Reis <ryanbr2903@gmail.com>
2024-03-18 09:31:47 -0300

    Exercício com IF

--- aula01/IMC.java
+++ aula01/IMC.java
@@ -4,20 +4,20 @@ public class IMC {
         Scanner nome1 = new Scanner(System.in);
         String nome;
         Scanner peso1 = new Scanner(System.in);
-        float peso;
+        Float peso;
         Scanner alt1 = new Scanner(System.in);
-        float alt;
+        Float alt;
 
         System.out.println("Insira seu nome:");
         nome = nome1.nextLine();
 
-        System.out.println("Insira seu peso :");
-        peso = peso1.nextLine();
+        System.out.println("Insira seu peso (em KG):");
+        peso = peso1.nextFloat();
 
-        System.out.println("Insira sua altura:");
-        alt = alt1.nextLine();
+        System.out.println("Insira sua altura (em CM):");
+        alt = alt1.nextFloat();
 
-        System.out.println(name, peso, alt);
+        System.out.println(nome, " tem ", alt,"cm de altura e pesa ", peso,"kg. Sua situação é xxx");
 
     }
 }
