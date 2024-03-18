9912e3157ae3987c895e9703c0b2bd5aea4fc8fe
Ryan Reis <ryanbr2903@gmail.com>
2024-03-18 09:22:29 -0300

    Aula sobre estrutura IF e Scanner

---
+++ aula01/IMC.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class IMC {
+    public static void main(String[] args) {
+        Scanner nome1 = new Scanner(System.in);
+        String nome;
+        Scanner peso1 = new Scanner(System.in);
+        float peso;
+        Scanner alt1 = new Scanner(System.in);
+        float alt;
+
+        System.out.println("Insira seu nome:");
+        nome = nome1.nextLine();
+
+        System.out.println("Insira seu peso :");
+        peso = peso1.nextLine();
+
+        System.out.println("Insira sua altura:");
+        alt = alt1.nextLine();
+
+        System.out.println(name, peso, alt);
+
+    }
+}
---
+++ aula01/If.java
@@ -0,0 +1,14 @@
+class If {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 20;
+
+        if (x > y) {
+            System.out.println("x é maior que y");
+        } else if (x == y) {
+            System.out.println("x e y são iguais");
+        } else {
+            System.out.println("y é maior que x");
+        }
+    }
+}
