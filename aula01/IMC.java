eae1a15242d365fd870018319d406b3cd67f6643
Ryan Reis <ryanbr2903@gmail.com>
2024-03-20 10:33:05 -0300

    Exercício IMC

--- aula01/IMC.java
+++ aula01/IMC.java
@@ -8,16 +8,19 @@ public class IMC {
         Scanner alt1 = new Scanner(System.in);
         Float alt;
 
-        System.out.println("Insira seu nome:");
+        System.out.println("Insira seu nome: ");
         nome = nome1.nextLine();
 
-        System.out.println("Insira seu peso (em KG):");
+        System.out.println("Insira seu peso (em KG): ");
         peso = peso1.nextFloat();
 
-        System.out.println("Insira sua altura (em CM):");
+        System.out.println("Insira sua altura (em CM): ");
         alt = alt1.nextFloat();
 
-        System.out.println(nome, " tem ", alt,"cm de altura e pesa ", peso,"kg. Sua situação é xxx");
+        float imc = peso / (alt * alt);
+
+        System.out.print(imc);
+        System.out.println(nome+" tem "+alt+"cm de altura e pesa "+peso+"kg. Sua situação é MAGREZA");
 
     }
 }
